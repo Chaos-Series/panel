2ed8235dcf89b599bf83739ca8e3f473ba4dece1
lambor590 <lambor590@gmail.com>
2024-01-25 00:07:22 +0100

    Crear función para obtener una cuenta de Riot por su PUUID

--- src/services/cuentas.js
+++ src/services/cuentas.js
@@ -1,42 +1,37 @@
 import axios from "axios";
 import api from "../../variables.json";
 import getPosition from "../utils/getPosition";
-
 import sendLog from "../utils/sendLog";
 import { toast } from "sonner";
 
 async function crearCuenta(valor, tag = "EUW", valorPrimaria, valorSecundaria, usuario, resolve, reject, cambioDatos) {
     axios
         .get(api.directorio + "cuentas/nombre=" + valor + "&tag=" + tag, { headers: { "x-auth-token": window.localStorage.getItem("token") } })
-        .then((cuentaComprobacion) => {
-            if (cuentaComprobacion.data.existe) {
+        .then((checkCuentaLoL) => {
+            if (checkCuentaLoL.data.existe) {
                 toast.error("Esta cuenta ya ha sido vinculada.");
-            } else if (cuentaComprobacion.data.existe == false && cuentaComprobacion.data.success == false) {
+            } else if (checkCuentaLoL.data.existe == false && checkCuentaLoL.data.success == false) {
                 toast.error("Esta cuenta no existe.");
             } else {
                 const principal = getPosition(valorPrimaria);
                 const secundaria = getPosition(valorSecundaria);
                 axios
-                    .post(
-                        api.directorio + "cuentas",
-                        {
-                            id_usuario: usuario.info["id_usuario"],
-                            invocador: cuentaComprobacion.data.result.gameName,
-                            tag: cuentaComprobacion.data.result.tagLine,
-                            puuid: cuentaComprobacion.data.result.puuid,
-                            linea_principal: principal,
-                            linea_secundaria: secundaria,
-                        },
-                        { headers: { "x-auth-token": window.localStorage.getItem("token") } }
-                    )
-                    .then(function (responsePost) {
+                    .post(api.directorio + "cuentas", {
+                        id_usuario: usuario.info["id_usuario"],
+                        invocador: checkCuentaLoL.data.result.gameName,
+                        tag: checkCuentaLoL.data.result.tagLine,
+                        puuid: checkCuentaLoL.data.result.puuid,
+                        linea_principal: principal,
+                        linea_secundaria: secundaria,
+                    }, { headers: { "x-auth-token": window.localStorage.getItem("token") } })
+                    .then((responsePost) => {
                         if (responsePost.data.status == 200) {
                             sendLog(usuario.info["id_usuario"], "Añadir Cuenta", {
                                 accion: "Añadir Cuenta",
                                 id_usuario: usuario.info["id_usuario"],
                                 invocador: valor,
                                 tag: tag,
-                                puuid: cuentaComprobacion.data.result.puuid,
+                                puuid: checkCuentaLoL.data.result.puuid,
                                 linea_principal: principal,
                                 linea_secundaria: secundaria,
                             });
@@ -47,10 +42,13 @@ async function crearCuenta(valor, tag = "EUW", valorPrimaria, valorSecundaria, u
                             reject();
                         }
                     })
-                    .catch(function () {
-                        reject();
+                    .catch((e) => {
+                        return e;
                     });
             }
+        })
+        .catch((e) => {
+            return e;
         });
 }
 
@@ -60,22 +58,35 @@ async function eliminarCuenta(data, resolve, reject, cambioDatos) {
             data: { id_cuenta: data.cuenta["id_cuenta"], id_usuario: data.cuenta["id_usuario"] },
             headers: { "x-auth-token": window.localStorage.getItem("token") },
         })
-        .then(function (responsePost) {
-            if (responsePost.data.status == 200) {
+        .then((response) => {
+            if (response.data.status == 200) {
                 sendLog(data.cuenta["id_usuario"], "Eliminar Cuenta", {
-                    accion: "Añadir Cuenta",
+                    accion: "Eliminar Cuenta",
                     id_cuenta: data.cuenta["id_cuenta"],
                 });
                 cambioDatos(true);
                 resolve();
-            } else if (responsePost.data.status == 500) {
+            } else if (response.data.status == 500) {
                 toast.error("Error. Avisa a la administración.");
                 reject();
             }
         })
-        .catch(function () {
-            reject();
+        .catch((e) => {
+            return e;
         });
 }
 
-export { crearCuenta, eliminarCuenta };
+async function getCuentaPorPUUID(puuid) {
+    axios
+        .get(api.directorio + "cuentas/puuid=" + puuid, {
+            headers: { "x-auth-token": window.localStorage.getItem("token") },
+        })
+        .then((response) => {
+            return response.data.existe ? response.data.result : false
+        })
+        .catch((e) => {
+            return e;
+        });
+}
+
+export { crearCuenta, eliminarCuenta, getCuentaPorPUUID };
